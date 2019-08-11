caff38b159a1d08b3fdfb46ae639a4349e6b23bd
hvrc <harshavardhan@rajmachikar.com>
2019-08-11 16:54:25 +0530

    heroku deployment changes

---
+++ fakeboy/home.py
@@ -0,0 +1,45 @@
+from fakeboy import app
+from fakeboy.scripts.main import *
+from flask import render_template, request
+
+ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
+
+@app.route("/", methods=["POST", "GET"])
+def home():
+    if request.method == "POST":
+        posts = [post[1] for post in request.form.items()]
+
+        human = Human(
+            full_name=posts[0],
+            gender=posts[1],
+            date_of_birth=posts[2],
+        )
+
+        photo = request.files["photo"]
+        photo_path = f"{ROOT_DIR}/static/database/images/{human.full_name_safe}_photo.png"
+        photo.save(photo_path)
+
+        card = AadhaarCard(
+            font_path=f"{ROOT_DIR}/static/fonts/calibri_bold.ttf",
+            devanagari_font_path=f"{ROOT_DIR}/static/fonts/adobe_devanagari_bold.otf",
+            aadhaar_number_font_path=f"{ROOT_DIR}/static/fonts/open_sans_bold.ttf",
+            male_front_template_path=f"{ROOT_DIR}/static/templates/male_front_template.png",
+            female_front_template_path=f"{ROOT_DIR}/static/templates/female_front_template.png",
+            back_template_path=f"{ROOT_DIR}/static/templates/back_template.png",
+            front_save_path=f"{ROOT_DIR}/static/database/ids/{human.full_name_safe}_front.png",
+            back_save_path=f"{ROOT_DIR}/static/database/ids/{human.full_name_safe}_back.png",
+            photo_path=photo_path,
+        )
+
+        card.create()
+        card.save()
+
+        return render_template(
+            "home.html",
+            full_name_safe=human.full_name_safe,
+        )
+
+    return render_template(
+        "home.html",
+        full_name_safe="",
+    )
